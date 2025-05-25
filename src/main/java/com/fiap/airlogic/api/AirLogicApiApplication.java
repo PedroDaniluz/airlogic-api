6ff37d37ef57f2b269bc607d7efeb8adbdbe2e80
Pedro Daniluz <pedrodaniluz04@gmail.com>
2025-05-24 21:06:09 -0300

    feat: initial comit - start project

---
+++ src/main/java/com/fiap/airlogic/api/AirLogicApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.airlogic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLogicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirLogicApiApplication.class, args);
+	}
+
+}
